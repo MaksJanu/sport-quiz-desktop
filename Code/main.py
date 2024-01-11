9473b20ac1f66590a2f7d979adedc484ebb28be5
Maks Januszewski <maksjanu@MacBook-Pro-Maks.local>
2024-01-11 14:30:16 +0100

    Dodanie tymczasowych grafik oraz widgetu

--- Code/main.py
+++ Code/main.py
@@ -9,10 +9,20 @@ app = QApplication(sys.argv)
 window = QWidget()
 window.setWindowTitle("Sport Quiz")
 window.setFixedWidth(1000)
+window.move(2700, 200)
 window.setStyleSheet("background: #161219;")
 
+#Zainicjalizowanie gridu pod widgety
+grid = QGridLayout()
+#Wrzucenie grafiki
+image = QPixmap("Images/logo.png")
+logo = QLabel()
+logo.setPixmap(image)
+#Dodanie widgetu do grida oraz umieszczenie go w kolumnie 0 i rzędzie 0
+grid.addWidget(logo, 0, 0)
+window.setLayout(grid)
 
-
+6.29
 
 
 
